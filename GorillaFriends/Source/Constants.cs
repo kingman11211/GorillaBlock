274cd8113dc89be6c63a301cd5f7905639331634
Zippy Boy <106349143+kingman11211@users.noreply.github.com>
2024-03-06 22:57:36 -0500

    Delete GorillaFriends directory

--- GorillaFriends/Source/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ModConstants
-{
-    class ModConstants
-    {
-        public const string modGUID = "net.rusjj.gorillafriends";
-        public const string modName = "Gorilla Friends!";
-        public const string modVersion = "1.2.1";
-    }
-}
